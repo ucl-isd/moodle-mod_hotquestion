9de9e13d1fd820850fe1d5f5bdb373237f3e7a7b
alrachels <drachels@drachels.com>
2016-10-06 11:13:54 -0500

    Bumped version for new release.

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016100300.1;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2016100600;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014111000;      // Requires Moodle 2.8 version.
 $plugin->cron      = 0;               // Period for cron to check this module (secs).
 $plugin->component = 'mod_hotquestion';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.1.0 (Build: 2016071600)"; // User-friendly version number.
+$plugin->release   = "3.1.0 (Build: 2016100600)"; // User-friendly version number.
