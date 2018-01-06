3858759bc01da23ff6c59401d62ebcf3a74e0a07
alrachels <drachels@drachels.com>
2018-01-06 11:39:03 -0600

    Changed to camel case.

--- module.js
+++ module.js
@@ -23,38 +23,38 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-M.mod_hotquestion = {};
+M.modHotquestion = {};
 
-M.mod_hotquestion.Y = {};
+M.modHotquestion.Y = {};
 
-M.mod_hotquestion.questionbox = {};
-M.mod_hotquestion.submitbutton = {};
+M.modHotquestion.questionbox = {};
+M.modHotquestion.submitbutton = {};
 
-M.mod_hotquestion.init = function(Y) {
-    M.mod_hotquestion.Y = Y;
+M.modHotquestion.init = function(Y) {
+    M.modHotquestion.Y = Y;
 
     // Init question box.
-    M.mod_hotquestion.questionbox = Y.one('#id_question');
-    M.mod_hotquestion.questionbox.on('valueChange', M.mod_hotquestion.questionchanged);
+    M.modHotquestion.questionbox = Y.one('#id_question');
+    M.modHotquestion.questionbox.on('valueChange', M.modHotquestion.questionchanged);
 
     // Init submit button.
-    M.mod_hotquestion.submitbutton = Y.one('#id_submitbutton');
-    if (M.mod_hotquestion.getquestion() == '') {
-        M.mod_hotquestion.submitbutton.set('disabled', 'disabled');
+    M.modHotquestion.submitbutton = Y.one('#id_submitbutton');
+    if (M.modHotquestion.getquestion() == '') {
+        M.modHotquestion.submitbutton.set('disabled', 'disabled');
     }
-    Y.on("submit", M.mod_hotquestion.submit, '#mform1');
+    Y.on("submit", M.modHotquestion.submit, '#mform1');
 
     // Bind toolbar buttons.
-    Y.on('click', M.mod_hotquestion.refresh, '.hotquestion_vote');
-    Y.on('click', M.mod_hotquestion.refresh, '.toolbutton');
+    Y.on('click', M.modHotquestion.refresh, '.hotquestion_vote');
+    Y.on('click', M.modHotquestion.refresh, '.toolbutton');
 
     // Bind io events.
-    Y.on('io:success', M.mod_hotquestion.iocomplete);
-    Y.on('io:failure', M.mod_hotquestion.iofailure);
+    Y.on('io:success', M.modHotquestion.iocomplete);
+    Y.on('io:failure', M.modHotquestion.iofailure);
 };
 
-M.mod_hotquestion.iocomplete = function(transactionid, response, arguments) {
-    var Y = M.mod_hotquestion.Y;
+M.modHotquestion.iocomplete = function(transactionid, response, arguments) {
+    var Y = M.modHotquestion.Y;
 
     // Update questions.
     var contentdiv = Y.one('#questions_list');
@@ -62,23 +62,23 @@ M.mod_hotquestion.iocomplete = function(transactionid, response, arguments) {
 
     // Clean up form if this is a submit IO.
     if (arguments.caller == 'submit') {
-        M.mod_hotquestion.questionbox.set('value', '');
-        M.mod_hotquestion.questionbox.removeAttribute('disabled');
-        M.mod_hotquestion.submitbutton.set('disabled', 'disabled');
+        M.modHotquestion.questionbox.set('value', '');
+        M.modHotquestion.questionbox.removeAttribute('disabled');
+        M.modHotquestion.submitbutton.set('disabled', 'disabled');
     }
 
     // Rebind buttons.
-    Y.on('click', M.mod_hotquestion.refresh, '.hotquestion_vote');
-    Y.on('click', M.mod_hotquestion.refresh, '.toolbutton');
+    Y.on('click', M.modHotquestion.refresh, '.hotquestion_vote');
+    Y.on('click', M.modHotquestion.refresh, '.toolbutton');
 };
 
-M.mod_hotquestion.iofailure = function(transactionid, response, arguments) {
-    M.mod_hotquestion.submitbutton.removeAttribute('disabled');
-    M.mod_hotquestion.questionbox.removeAttribute('disabled');
+M.modHotquestion.iofailure = function(transactionid, response, arguments) {
+    M.modHotquestion.submitbutton.removeAttribute('disabled');
+    M.modHotquestion.questionbox.removeAttribute('disabled');
     alert(M.str.hotquestion.connectionerror);
 };
 
-M.mod_hotquestion.refresh = function(e) {
+M.modHotquestion.refresh = function(e) {
     e.preventDefault();
 
     var data = e.currentTarget.get('href').split('?', 2)[1];
@@ -91,17 +91,17 @@ M.mod_hotquestion.refresh = function(e) {
         }
     };
 
-    var request = M.mod_hotquestion.Y.io('view.php', cfg);
+    var request = M.modHotquestion.Y.io('view.php', cfg);
 };
 
-M.mod_hotquestion.getquestion = function() {
-    var question = M.mod_hotquestion.questionbox.get('value');
+M.modHotquestion.getquestion = function() {
+    var question = M.modHotquestion.questionbox.get('value');
     return YAHOO.lang.trim(question);
 };
 
-M.mod_hotquestion.questionchanged = function(e) {
-    var question = M.mod_hotquestion.getquestion();
-    var submitbutton = M.mod_hotquestion.submitbutton;
+M.modHotquestion.questionchanged = function(e) {
+    var question = M.modHotquestion.getquestion();
+    var submitbutton = M.modHotquestion.submitbutton;
     if (question == '') {
         submitbutton.set('disabled', 'disabled');
     } else {
@@ -109,20 +109,20 @@ M.mod_hotquestion.questionchanged = function(e) {
     }
 };
 
-M.mod_hotquestion.submit = function(e) {
+M.modHotquestion.submit = function(e) {
     e.preventDefault();
 
-    var question = M.mod_hotquestion.getquestion();
+    var question = M.modHotquestion.getquestion();
     if (question == '') {
         return; // Ignore empty question.
     }
 
     // To avoid multiple clicks and editing.
-    M.mod_hotquestion.submitbutton.set('disabled', 'disabled');
-    M.mod_hotquestion.questionbox.set('disabled', 'disabled');
+    M.modHotquestion.submitbutton.set('disabled', 'disabled');
+    M.modHotquestion.questionbox.set('disabled', 'disabled');
 
     // Get all input components.
-    var inputs = M.mod_hotquestion.Y.all('#mform1 input');
+    var inputs = M.modHotquestion.Y.all('#mform1 input');
 
     // Construct post data.
     var data = '';
@@ -143,5 +143,5 @@ M.mod_hotquestion.submit = function(e) {
             caller: 'submit',
         }
     };
-    var request = M.mod_hotquestion.Y.io('view.php', cfg);
+    var request = M.modHotquestion.Y.io('view.php', cfg);
 };
