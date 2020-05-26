95dbf5c566d2c08db405df9c9c68c99188175e6f
drachels <drachels@drachels.com>
2020-05-26 09:32:39 -0500

    Added new fields to the backup.

--- backup/moodle2/backup_hotquestion_stepslib.php
+++ backup/moodle2/backup_hotquestion_stepslib.php
@@ -53,7 +53,9 @@ class backup_hotquestion_activity_structure_step extends backup_activity_structu
         // Define each element separated.
         $hotquestion = new backup_nested_element('hotquestion', array('id'), array(
             'name', 'intro', 'introformat', 'submitdirections', 'timecreated',
-            'timemodified', 'teacherpriorityvisibility', 'heatvisibility', 'anonymouspost', 'approval'));
+            'timemodified', 'timeopen', 'timeclose', 'questionlabel', 'teacherpriorityvisibility',
+            'teacherprioritylabel', 'heatvisibility', 'heatlabel', 'heatlimit', 'anonymouspost',
+            'approval', 'approvallabel', 'removelabel'));
 
         $questions = new backup_nested_element('questions');
 
