8364f0cf803d289ae65d871be1df4472f0e6aea9
drachels <drachels@drachels.com>
2020-06-06 12:06:53 -0500

    Bumped version.

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020052900;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2020060600;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014111000;      // Requires Moodle 2.8 version.
 $plugin->cron      = 0;               // Period for cron to check this module (secs).
 $plugin->component = 'mod_hotquestion';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.9.0 (Build: 2020052900)"; // User-friendly version number.
+$plugin->release   = "3.9.0 (Build: 2020060600)"; // User-friendly version number.
